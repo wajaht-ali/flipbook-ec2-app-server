42b01341966e093129069b0003881cc62dbbaa9d
wajaht-ali <contact.chwajahat@gmail.com>
2025-06-18 10:25:15 +0500

    refactor: envirnoment vars config

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import app from "./src/app.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const PORT = process.env.PORT || 8080;
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
